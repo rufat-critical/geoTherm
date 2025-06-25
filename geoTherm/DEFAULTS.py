ec119ce2f246a4077990248d58fd3b9a096837a6
Rufat <rufat@criticalenergy.co>
2025-06-25 15:53:05 -0700

    Updated Defaults to HEOS

--- geoTherm/DEFAULTS.py
+++ geoTherm/DEFAULTS.py
@@ -6,7 +6,7 @@ class ThermSettings:
     """Default settings for geoTherm thermodynamic calculations."""
     input_units: Literal['SI'] = 'SI'
     output_units: Literal['SI'] = 'SI'
-    EoS: Literal['REFPROP', 'HEOS'] = 'REFPROP'
+    EoS: Literal['REFPROP', 'HEOS'] = 'HEOS'
     model: Literal['coolprop'] = 'coolprop'
 
 # Create the default instance
